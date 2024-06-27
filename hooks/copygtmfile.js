7d590704ff705fad0da9112484b9f16b111a7156
Casapia <angel.casapia@gmail.com>
2024-06-27 13:31:05 +0200

    chore: Update hook to copy GTM-TMTPTRLZ_v2.json file for iOS container

--- hooks/copygtmfile.js
+++ hooks/copygtmfile.js
@@ -5,15 +5,26 @@ const path = require("path");
 
 module.exports = function (context) {
   const rootdir = context.opts.projectRoot;
-  const srcContainerDir = path.join(rootdir, "src", "ios", "container");
-  const srcFile = path.join(srcContainerDir, "GTM-TMTPTRLZ_v2.json");
+  const pluginDir = context.opts.plugin.dir;
+  const srcFile = path.join(
+    pluginDir,
+    "src",
+    "ios",
+    "container",
+    "GTM-TMTPTRLZ_v2.json"
+  );
   const iosPlatformDir = path.join(rootdir, "platforms", "ios");
   const destContainerDir = path.join(iosPlatformDir, "container");
   const destFile = path.join(destContainerDir, "GTM-TMTPTRLZ_v2.json");
 
+  console.log(`Looking for source file: ${srcFile}`);
+
   if (fs.existsSync(srcFile)) {
+    console.log(`Source file found: ${srcFile}`);
+
     if (!fs.existsSync(destContainerDir)) {
       fs.mkdirSync(destContainerDir, { recursive: true });
+      console.log(`Created destination directory: ${destContainerDir}`);
     }
 
     fs.copyFileSync(srcFile, destFile);
