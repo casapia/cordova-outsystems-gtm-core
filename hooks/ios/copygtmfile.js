d8065d22c08614cfe3ea84368bb9129b7eddaa3b
Angel Casapía <angel.casapia@gmail.com>
2024-07-04 00:35:47 +0200

    Revert "chore: Update hook to copy GTM-TMTPTRLZ.json file for iOS container"
    This reverts commit 3a280d732a7f9698546e0a7562b9ab80b4612ba4.

--- hooks/ios/copygtmfile.js
+++ hooks/ios/copygtmfile.js
@@ -5,7 +5,7 @@ const path = require("path");
 const xcode = require("xcode");
 const { ConfigParser } = require("cordova-common");
 
-const gtmContainerName = "GTM-TMTPTRLZ.json";
+const gtmContainerName = "GTM-TMTPTRLZ_v2.json";
 
 module.exports = function (context) {
   function getAppName(context) {
